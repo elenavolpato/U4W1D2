c1cee380725da4584aa47dc087dbfc339594192e
raposinha <elena@raposinha.dev>
2026-03-10 16:57:10 +0100

    finishes exercise 3

--- src/es3/Main.java
+++ src/es3/Main.java
@@ -1,6 +1,31 @@
 package es3;
 
+
+//Exercise #3 – while
+//Write a program that asks the user to enter a string and splits it into characters separated by a comma. The program repeats until the user enters the string ":q".
+
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        while(!input.equals(":q")) {
+            System.out.print("Enter a string (\":q\" to quit): ");
+            input = scanner.nextLine();
+
+            if (!input.equals(":q")) {
+                String result = "";
+                for (int i = 0; i < input.length(); i++) {
+                    result += input.charAt(i);
+                    if (i < input.length() - 1) {
+                        result += ",";
+                    }
+                }
+                System.out.println(result);
+            }
+
+        }
     }
 }
